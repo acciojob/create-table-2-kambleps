5cd041f534f31ada9eda12f8c28cdee3469eb6dd
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-21 18:44:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function createTable() {
     //Write your code here
-  
+
+const rn = prompt('Input number of rows');
+const cn = prompt('Input number of columns');
+
+const myTable = document.getElementById('myTable');
+
+for (let i = 0; i < rn; i++) {
+const row = document.createElement('tr');
+
+for (let j = 0; j < cn; j++) {
+const cell = document.createElement('td');
+const cellText = document.createTextNode(Row-${i} Column-${j});
+row.appendChild(cell);
+cell.appendChild(cellText);
+
+}
+//Row-${Math.floor(index / 2)} Column-${index % 2}
+myTable.appendChild(row);
+}
 }
